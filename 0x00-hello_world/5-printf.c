a3224a0b8b8d6b63e26a4c77ea25838e3970f1ac
Nana <nanakzy@gmail.com>
2023-07-13 05:55:47 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a message
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
