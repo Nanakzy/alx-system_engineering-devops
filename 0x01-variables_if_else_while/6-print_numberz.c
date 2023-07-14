53e565ca3121e88dd62ea693ad9dcdf71fd9973b
Nana <nanakzy@gmail.com>
2023-07-14 08:38:48 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * description - prints digits in putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
