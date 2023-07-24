68738d67716065960d72a24b15d70f1220c3d275
Nana <nanakzy@gmail.com>
2023-07-24 08:59:13 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts - prints a string, followed by a new line, to stdout
- * @str - the string to be printed
+ * @str: the string to be printed
  * Return: the string length
  */
 void _puts(char *str)
