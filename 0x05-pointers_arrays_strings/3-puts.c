fe25f98ff7142d1a3f81c24077a0816b390ba5de
Nana <nanakzy@gmail.com>
2023-07-24 08:55:55 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str - the string to be printed
+ * Return: the string length
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		putchar(str[a]);
+		a++;
+	}
+	putchar('\n');
+}
