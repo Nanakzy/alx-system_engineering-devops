fa7fdfd08940078a83e927ebeed5be364996a777
Nana <nanakzy@gmail.com>
2023-07-31 23:01:12 +0200

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - copies memory area
+ * @dest: output
+ * @src: memory area pointer
+ * @n: number of times
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
