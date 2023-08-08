dda42f0160aa8f7b234f9af4eec6c16374a06814
Nana <nanakzy@gmail.com>
2023-08-08 08:22:36 +0200

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: the number of arguments
+ * @argv: the string of arguments
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
