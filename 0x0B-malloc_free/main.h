2d6caabebe8907f32d136da878df64c863675671
Nana <nanakzy@gmail.com>
2023-08-08 14:32:08 +0200

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,10 @@
 #include <stdlib>
 
 int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif
