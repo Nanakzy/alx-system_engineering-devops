146b63018eecaa0e0b7249a12897f1ec4462c93e
Nana <nanakzy@gmail.com>
2023-08-09 12:10:07 +0200

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,11 +4,42 @@
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers
- * @grid: the address of dimensional grid
  * @width: width of grid
  * @height: height of grid
  * Return: Nothing
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **s;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	s = malloc(sizeof(int *) * height);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		s[i] = malloc(sizeof(int) * width);
+
+		if (s[i] == NULL)
+		{
+			for (j = i; j < 0; j++)
+				free(s[j]);
+		}
+		free(s);
+		return (NULL);
+	}
+
+	for (j = 0; i < width; j++)
+	{
+		s[i][j] = 0;
+	}
+	return (s);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
