677d8e5f68049217122429dcbdb91acbef5025ad
Nana <nanakzy@gmail.com>
2023-08-14 22:42:47 +0200

    2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,14 +2,14 @@
 #define DOG_H
 
 /**
- * struct dog - dogs details
- * @name: name of dog
- * @age: age of dog
- * @owner: name of dog owner
+ * struct dog - dog's details
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
  *
- * Description: longer description
+ * Description: This struct represents details about a dog.
  */
-strucy dog
+struct dog
 {
 	char *name;
 	float age;
@@ -17,15 +17,16 @@ strucy dog
 };
 
 /**
- * dog_t- typedef for struct dog
+ * dog_t - typedef for struct dog
  */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-typedef struct dog dog_t;i
+char *_strcpy(char *dest, const char *src);
+int _strlen(const char *s);
 
 #endif
+
