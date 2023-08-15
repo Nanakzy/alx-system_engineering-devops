7830c4d01b55033f677f27c96cfd5db827bcde81
Nana <nanakzy@gmail.com>
2023-08-15 07:32:12 +0200

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_fibonacci_sequence - prints the first 98 Fibonacci numbers
+ * @n: the number to be printed
+ * Return: 0 success
+ */
+void print_fibonacci_sequence(int n)
+{
+	int a = 1, b = 2, c;
+
+	printf("%d, %d", a, b);
+
+	for (int count = 2; count < n; ++count)
+	{
+		c = a + b;
+		printf(", %d", c);
+		a = b;
+		b = c;
+	}
+}
+
+/**
+ * main - prints a sequence
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_fibonacci_sequence(98);
+	printf("\n");
+
+	return (0);
+}
