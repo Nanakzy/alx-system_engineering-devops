563db749a8891d225228c876b91916a14feae193
Nana <nanakzy@gmail.com>
2023-08-17 06:47:23 +0200

    function_pointers.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTERS.H
+#define FUNCTION_POINTERS.H
+
+#include "funtion_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
