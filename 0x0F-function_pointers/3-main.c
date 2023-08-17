43539a4138673a3793bd4221f5c6923822fc238c
Nana <nanakzy@gmail.com>
2023-08-17 20:36:52 +0200

    3-get_op_func.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -32,9 +32,8 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		return (100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 
-		printf("%d\n", get_op_func(op)(num1, num2));
-
-		}
 	return (0);
 }
