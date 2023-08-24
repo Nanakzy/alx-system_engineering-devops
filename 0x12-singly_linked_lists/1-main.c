6a43fdeec72651db255de7bf0afa4496cd525a3e
Nanakzy <nanakzy@gmail.com>
2023-08-24 10:22:59 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/1-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	list_t *head;
+
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,14 @@
 
 #include <unistd.h>
 #include <stdio.h>
-
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
