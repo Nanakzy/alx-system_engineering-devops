41e035edb78263daeeb3766705744b5807b5b3d9
Nana <nanakzy@gmail.com>
2023-10-07 11:31:14 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,12 +10,23 @@ void print_binary(unsigned long int n)
 {
 	int num_bits = sizeof(unsigned long int) * 8;
 	int i;
+	int leading_zero = 1;
 
 	for (i = num_bits - 1; i >= 0; i--)
 	{
 		unsigned long int mask = 1UL << i;
 
-		putchar((n & mask) ? '1' : '0');
+		if ((n & mask) || !leading_zero)
+		{
+			putchar((n & mask) ? '1' : '0');
+			leading_zero = 0;
+		}
 	}
+
+	if (leading_zero)
+	{
+		putchar('0');
+	}
+
 	putchar('\n');
 }
