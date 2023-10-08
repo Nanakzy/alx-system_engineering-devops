d17c117868c06c129a34c51610468a8603adb9e2
Nana <nanakzy@gmail.com>
2023-10-08 05:16:40 -0700

    main.h

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,88 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+
+int copy_file(const char *file_from, const char *file_to);
+
+/**
+ * main - checks if number of arguments are correct
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: result
+ */
+int main(int argc, char *argv[])
+{
+	const char *file_from;
+	const char *file_to;
+	int result;
+	int fd_from;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd_from = open(file_from, O_RDONLY);
+
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	result = copy_file(file_from, file_to);
+
+
+	if (result == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+	}
+	else if (result == 100)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd 3\n");
+		exit(100);
+	}
+	close(fd_from);
+	return (0);
+}
+
+/**
+ * copy_file - copies the content of a file to another file
+ * @fd_from: file to be copied
+ * @file_to: file to copy into
+ *
+ * Return: 0 success
+ */
+int copy_file(int fd_from, const char *file_to)
+{
+	int fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read;
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		return (99);
+	}
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (write(fd_to, buffer, bytes_read) != bytes_read)
+		{
+			close(fd_from);
+			close(fd_to);
+			return (99);
+		}
+	}
+
+	close(fd_to);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+int copy_file(int fd_from, const char *file_to);
 
 #endif
