f831d753a2fef1ad25ed1f3f1289aca0c80344ad
Nana <nanakzy@gmail.com>
2023-10-11 03:01:44 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,12 +3,17 @@
 
 #define BUFF_SIZE 1024
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
+#include <string.h>
+#include <errno.h>
 
-
+void print_elf_header(const Elf64_Ehdr *elf_header);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
