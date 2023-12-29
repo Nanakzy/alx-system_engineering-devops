2da73159018c76af0c72955fbd55b060b8add936
Nana <nanakzy@gmail.com>
2023-12-29 12:11:33 -0800

    task1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,8 @@
+/**
+ * hash_djb2 - implementing the djb2 algorithm.
+ * @str: the string pointer
+ * Return: result
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
