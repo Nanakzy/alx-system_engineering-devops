f6ce65e93cd810bf310f1268667b42b390b3f5c6
Nana <nanakzy@gmail.com>
2023-12-30 05:47:24 -0800

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_create - Creates a hash table
  * @size: Size of the array
- * Return: Pointer to the newly created hash table, or NULL if something went wrong
+ * Return: Pointer to the newly created hash table,NULL if something is wrong
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
