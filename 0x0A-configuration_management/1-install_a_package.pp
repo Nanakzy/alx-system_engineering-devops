ea12b9570c4eedda47477048729b740468dc70da
Nana <nanakzy@gmail.com>
2024-03-22 22:28:46 +0200

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
-#!/usr/bin/puppet
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Define a package resource to install Flask version 2.1.0 using pip3
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Define a package resource to install Werkzeug version 2.1.1 using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+# Notice will be shown when Flask package is created
+notify { 'Flask_package_created':
+  message  => 'Package[Flask]/ensure: created',
 }
