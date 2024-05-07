5af261fbabcfbe828b403320095830ef25fe5c05
Nanakzy <nanakzy@gmail.com>
2024-05-07 03:58:01 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,16 +5,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
+    """ Function that queries the Reddit Api """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
         return 0
