5b661699aad1281a01255d906b43694b55e6da27
Nanakzy <nanakzy@gmail.com>
2024-05-16 16:57:50 +0000

    0-the_sky_is_the_limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,8 @@
 # Increase the ULIMIT of the default file
-<<<<<<< HEAD
-
-exec { 'nginx-restart':
-  command => '/etc/init.d/nginx restart',
-  path    => '/etc/init.d/',
-  require => Exec['fix--for-nginx'],
-}
-=======
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
->>>>>>> d0b9d05aab4aadde7eca80d8ce5515b015554b4f
 
 # Restart Nginx
 exec { 'nginx-restart':
